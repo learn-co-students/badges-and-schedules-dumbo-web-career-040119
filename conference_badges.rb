f67d9747c6a61d7463bc7635eaf62b8d93176450
cogilvy <chrisogilvy@cox.net>
2019-03-18 18:57:35 -0700

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,6 @@
 # Write your code here.
+speaker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+room = [1, 2, 3, 4, 5, 6, 7]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
@@ -16,6 +18,8 @@ end
 
 def assign_rooms(speakers)
   room_assignments = []
+  # syntax for each_with_index is:
+  #        each_with_index do |object, index|     #
   speakers.each_with_index do |name, room|
     room_assignments.push "Hello, #{name}! You'll be assigned to room #{room + 1}!"
   end
@@ -27,6 +31,7 @@ def printer(speakers)
   batch_badge_creator(speakers).each do |name|
     puts name
   end
+  # name is just a placeholder for each sentence #
   assign_rooms(speakers).each do |name|
     puts name
   end
